c76ffac9ce3f49709066369984839739c78d2054
Karol Dytman <dytmankarol@wp.pl>
2023-02-22 20:28:27 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+{
+    const tasks = [
+        { content: "zadanie 1" },
+        { content: "zadanie 2" },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+    }
+    const init = () => {
+
+    };
+    init();
+}
