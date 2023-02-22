12c63d1eeef0deed2910cfc9ca5704ad50f2e59b
Karol Dytman <dytmankarol@wp.pl>
2023-02-22 22:48:07 +0100

    adding a button "done"

--- js/script.js
+++ js/script.js
@@ -15,42 +15,52 @@
         for (const task of tasks) {
             htmlString += `
             <li${task.done ? "" : ""}>
-            <button class=".tasks__button--remove">🗑</button>
+            <button class="js-done tasks__button--done">✔<?button> 
+            <button class="js-remove tasks__button--remove">🗑</button>
             ${task.content}
             </li>`;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                remuveTask(index);
+            });
+        });
+    };
+    const remuveTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    }
+
+    const addnewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
     };
 
-    const removeButtons = document.querySelectorAll(".tasks__button--remove");
+    const oneFormSubmit = (event) => {
+        event.preventDefault();
 
-    removeButtons.forEach((removeButton, index) => {
-        removeButton.addEventListener("click", () => {
-            tasks.splice(index);
-        });
-    });
+        const newTaskContent = document.querySelector(".js-newTask").value;
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addnewTask(newTaskContent);
+    };
 
     const init = () => {
         render();
 
         const form = document.querySelector(".js-form");
 
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-
-            const newTaskContent = document.querySelector(".js-newTask").value;
-
-            if (newTaskContent === "") {
-                return;
-            }
-
-            tasks.push({
-                content: newTaskContent,
-            });
-
-            render();
-        });
+        form.addEventListener("submit", oneFormSubmit);
     };
     init();
 }
