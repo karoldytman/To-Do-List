6f58f6371b58332738dfe9c2ac99cd8fae6fdb92
Karol Dytman <dytmankarol@wp.pl>
2023-02-22 21:08:45 +0100

    fonction form

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
 {
-    const tasks = [
-        { content: "zadanie 1" },
-        { content: "zadanie 2" },
+    const tasks = [{
+            content: "zadanie 1",
+            done: false,
+        },
+        {
+            content: "zadanie 2",
+            done: true,
+        },
     ];
 
     const render = () => {
         let htmlString = "";
-    }
-    const init = () => {
 
+        for (const task of tasks) {
+            htmlString += `
+            <li${task.done ? "" : ""}>
+            ${task.content}
+            </li>`;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+        });
     };
     init();
 }
