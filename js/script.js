fa65094e7ac094877b3f22ca59befa778b727489
Karol Dytman <dytmankarol@wp.pl>
2023-02-22 21:39:07 +0100

    button to delete the tasks

--- js/script.js
+++ js/script.js
@@ -15,12 +15,21 @@
         for (const task of tasks) {
             htmlString += `
             <li${task.done ? "" : ""}>
+            <button class=".tasks__button--remove">🗑</button>
             ${task.content}
             </li>`;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
+
+    const removeButtons = document.querySelectorAll(".tasks__button--remove");
+
+    removeButtons.forEach((removeButton, index) => {
+        removeButton.addEventListener("click", () => {
+            tasks.splice(index);
+        });
+    });
 
     const init = () => {
         render();
@@ -29,6 +38,18 @@
 
         form.addEventListener("submit", (event) => {
             event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value;
+
+            if (newTaskContent === "") {
+                return;
+            }
+
+            tasks.push({
+                content: newTaskContent,
+            });
+
+            render();
         });
     };
     init();
